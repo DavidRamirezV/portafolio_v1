9ae9de22bba528025979fdb61c453abe9755cb4e
Deivi <david.ramirez.v@usach.cl>
2023-05-16 21:02:52 -0400

    start

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap/dist/js/bootstrap.js"
+
+createApp(App).mount('#app')
