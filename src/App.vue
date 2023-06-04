29b61782f9c4f309456de7680fbbc14308ba4e9a
Deivi <david.ramirez.v@usach.cl>
2023-06-04 00:36:09 -0400

    fixing positions

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,60 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid body-default">
 
-    <div class="row">
+    <div class="row ">
       <div class="col-2 sidebar_st">
+        <!--
+  Top
+ -->
+        <div class="row py-5 m-3">
+          <img src="./assets/img/profile_magenta.png" id="profileimg" class="card-img-top" alt="Profile">
+        </div>
+        <!--
+  Middle
+ -->
+        <div class="py-5">
+          <div class="pice-border">
+            <a class="btn" href="#"> TESTING 1</a>
+          </div>
+          <div class="pice-border">
+            <a class="btn" href="#"> TESTING 2</a>
+          </div>
+          <div class="pice-border">
+            <a class="btn" href="#"> TESTING 3</a>
+          </div>
+          <div class="pice-border">
+            <a class="btn" href="#"> test 4</a>
+          </div>
+          <div class="pice-border">
+            <a class="btn" href="#"> test 5</a>
+          </div>
+        </div>
+        <!--
+Footer
+ -->
+        <div class="row">
+          <div class="card-footer text-muted py-3">
+            <div class="row text-center mx-4">
+              <div class="col">
+                <a href="https://www.linkedin.com/in/david-antonio-ramirez-vicuna/" target="_blank">
+                  <img src="./assets/img/linkedin.png" alt="Linkedin" height="50px" class="zoom">
+                </a>
+              </div>
+              <div class="col">
+                <a href="https://github.com/DavidRamirezV" target="_blank">
+                  <img src="./assets/img/github.png" alt="GitHub" height="50px" class="zoom">
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <img src="https://i.ibb.co/PtSkvSQ/katwall.jpg" class="card-img-top" alt="Preview">
-
-        <div class="pice-border">
-          <a class="btn" href="#"> TESTING 1</a>
-        </div>
-        <div class="pice-border">
-          <a class="btn" href="#"> TESTING 2</a>
-        </div>
-        <div class="pice-border">
-          <a class="btn" href="#"> TESTING 3</a>
-        </div>
-        <div class="pice-border">
-          <a class="btn" href="#"> test 4</a>
-        </div>
-        <div class="pice-border">
-          <a class="btn" href="#"> test 5</a>
         </div>
 
 
-        <div class="card-footer text-muted">
-          2 days ago
-        </div>
+
+
+
+
 
 
       </div>
@@ -36,7 +65,7 @@
         quidem nihil neque iusto consectetur, veritatis provident asperiores, molestias officiis sapiente facere fuga
         veniam maiores?
 
-        
+
       </div>
 
 
@@ -60,21 +89,28 @@ export default {
   name: 'App',
   components: {
 
+  },
+  methods: {
   }
 }
+
+
 </script>
 
 <style lang="scss">
-.sidebar_st {
-  background-color: #1f1f1f;
-  height:100vh;
-  ul {
-    li {
-      background-color: rgb(141, 114, 139);
-    }
-  }
+.body-default {
+  background-color: #272727;
+  color: #fff;
 }
 
+//SIDEBAR
+
+.sidebar_st {
+  background-color: #1f1f1f;
+  height: 100vh;
+}
+
+//RGB LIGHT EFFECT
 .pice-border {
   background: rgb(250, 6, 252);
   background: linear-gradient(90deg, rgba(250, 6, 252, 1) 0%, rgba(158, 61, 255, 1) 25%, rgba(0, 249, 255, 1) 50%, rgba(158, 61, 255, 1) 75%, rgba(250, 6, 252, 1) 100%);
@@ -136,4 +172,20 @@ export default {
   }
 
 }
+
+//ZOOM
+.zoom {
+  transition: transform .2s;
+}
+
+.zoom:hover {
+  -ms-transform: scale(1.2);
+  /* IE 9 */
+  -webkit-transform: scale(1.2);
+  /* Safari 3-8 */
+  transform: scale(1.2);
+}
+
+
+//photo morph
 </style>
